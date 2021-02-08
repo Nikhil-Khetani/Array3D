a71bc7020aa5df6955557863a2d9196cb406816c
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-02-08 19:25:54 +0000

    Created array3d.py file

---
+++ array3d.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+arr = np.array([[[1,4,5],[4,3,8],[2,7,2]],[[2,2,4],[8,5,2],[6,4,9]],[[5,8,2],[5,8,2],[5,9,2]]])
+print(arr.shape)
+
+def plot3darray(arr):
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    x=np.arange
+    return
